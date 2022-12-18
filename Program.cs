8f3e3439a502e759b4970628a4183efc6a413f41
Marcus Moraes <marcusmoraes2010@hotmail.com>
2022-12-18 14:12:08 -0300

    test: run all methods from Nokia instance

--- Program.cs
+++ Program.cs
@@ -3,7 +3,13 @@
 // TODO: Realizar os testes com as classes Nokia e Iphone
 
 Iphone iphoneX = new Iphone("559999999", "X", "555", 32);
+
 iphoneX.Ligar();
 iphoneX.ReceberLigacao();
 iphoneX.InstalarAplicativo("WhatsApp");
 
+Nokia NokiaG21 = new Nokia("5512312312", "G21", "556", 32);
+
+NokiaG21.Ligar();
+NokiaG21.ReceberLigacao();
+NokiaG21.InstalarAplicativo("WhatsApp");

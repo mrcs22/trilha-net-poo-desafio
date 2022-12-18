88efdcc0d1518741336bc28287dc3487d76b8e7b
Marcus Moraes <marcusmoraes2010@hotmail.com>
2022-12-18 14:06:24 -0300

    test: run all methods from Iphone instance

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphoneX = new Iphone("559999999", "X", "555", 32);
+iphoneX.Ligar();
+iphoneX.ReceberLigacao();
+iphoneX.InstalarAplicativo("WhatsApp");
+
